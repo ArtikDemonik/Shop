267d76bc70ae6d231509f957b921be6ed13d3db3
orderyoo <markovmaks895@gmail.com>
2023-01-24 21:28:57 +0300

    у

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("хоч спать");
+        System.out.println("Hello");
+        System.out.println("хочy спать");
     }
 }
