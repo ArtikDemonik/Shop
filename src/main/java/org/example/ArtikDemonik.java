9139e8a76b8c4fa1230f2def6daa0de2a73db98c
ArtikDemonik <artikdemonik@yandex.ru>
2023-01-24 22:01:03 +0300

    add ArtikDemonik.java

---
+++ src/main/java/org/example/ArtikDemonik.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ArtikDemonik {
+    private static final int stressed = 100;
+    public static int get_stressed(){
+        return ArtikDemonik.stressed;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("хоч спать");
+        System.out.printf("my stress level is %d", ArtikDemonik.get_stressed());
     }
 }
